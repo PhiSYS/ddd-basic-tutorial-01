15643edf049ae16700be3490e7c938e74ef8c2fd
David Strencsev <phisys.digitalys@gmail.com>
2021-09-29 08:25:20 +0200

    added findManyByAuthorId to BookRepository

--- src/Domain/Model/Book/BookRepository.php
+++ src/Domain/Model/Book/BookRepository.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 namespace PhiSYS\Domain\Model\Book;
 
+use PhiSYS\Domain\Model\Author\ValueObject\AuthorId;
 use PhiSYS\Domain\Model\Book\ValueObject\BookId;
 
 interface BookRepository
 {
     public function find(BookId $bookId): ?Book;
+
+    public function findManyByAuthorId(AuthorId $authorId): BookCollection;
 
     public function save(Book $book): void;
 }
