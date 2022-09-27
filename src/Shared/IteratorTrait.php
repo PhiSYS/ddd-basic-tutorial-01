517629538571b7ce89eb9a2d7b10690d260afd45
PhiSYS <phisys.digitalys@gmail.com>
2022-09-27 17:20:20 +0200

    Fixes new phpcs and phpstan

--- src/Shared/IteratorTrait.php
+++ src/Shared/IteratorTrait.php
@@ -8,27 +8,27 @@ trait IteratorTrait
     private $items;
     private $current;
 
-    public function current()
+    public function current(): mixed
     {
         return $this->items[$this->current];
     }
 
-    public function next()
+    public function next(): void
     {
         ++$this->current;
     }
 
-    public function key()
+    public function key(): int
     {
         return $this->current;
     }
 
-    public function valid()
+    public function valid(): bool
     {
         return array_key_exists($this->current, $this->items);
     }
 
-    public function rewind()
+    public function rewind(): void
     {
         $this->current = 0;
     }
